bb0d76998e2e313293a747e88a6f2db4f03c5033
Frank Pfotenhauer <frank.pfotenhauer@msb.nrw.de>
2026-06-21 14:38:04 +0200

    NSIS Setting for Windows

--- electron/main.cjs
+++ electron/main.cjs
@@ -23,6 +23,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
     height: 900,
+    icon: path.join(__dirname, '../build/icon.png'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
